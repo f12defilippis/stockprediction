e6bd7b0c4fa78595cc2bb8bc7283b2b774b9b736
f12defilippis <francesco.defilippis@gmail.com>
2018-08-15 22:22:25 +0200

    Chandelier Exit Service

---
+++ src/main/java/it/f12/stockprediction/service/ChandelierExitService.java
@@ -0,0 +1,57 @@
+package it.f12.stockprediction.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import it.f12.stockprediction.orm.entity.Quote;
+
+@Service
+public class ChandelierExitService {
+
+	@Autowired
+	private QuoteService quoteService;
+	
+	@Autowired
+	private AverageTrueRangeService averageTrueRangeService;
+	
+	public Double calculateLong(Quote quote,  int numDays)
+	{
+		return calculate(quote, "l", numDays);
+	}
+
+	public Double calculateShort(Quote quote,  int numDays)
+	{
+		return calculate(quote, "s", numDays);
+	}
+
+	
+	private Double calculate(Quote quote, String longShort, int numDays)
+	{
+		
+		Double exit = 0.0;
+		
+		List<Quote> quotes = quoteService.getLastNQuotesUntilDate(quote.getDateOfQuote(), numDays);
+
+		Double max = quoteService.getMaxofMaxValues(quotes);
+		
+		Double atr = averageTrueRangeService.calculate(quote, numDays);
+				
+		if(longShort.equals("l"))
+		{
+			exit = max - (atr*3);
+		}else
+		{
+			exit = max + (atr*3);
+		}
+		
+		
+		return exit;
+		
+	}
+	
+	
+	
+	
+}
